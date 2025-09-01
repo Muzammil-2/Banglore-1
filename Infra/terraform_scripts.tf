6e51f4d5abbf27d955d8f35d9b7358d5dd417515
Muzammil-2 <109243844+Muzammil-2@users.noreply.github.com>
2025-09-01 15:42:33 +0000

    update infra

--- Infra/terraform_scripts.tf
+++ Infra/terraform_scripts.tf
@@ -4,15 +4,25 @@ google_compute_network "network"{
 }
 
 google_compute_subnetwork "sunetwork" {
-    network =
-    name =
-    cidr = 
-    availability_zone =
+    network = google_compute_network.network.selflink
+    name = pub_subnet
+    cidr =  ["172.16.1.0/24"]
+    availability_zone = "us-west-1a
 }
 
+google_cloud_router "rt-gcp"  {
+    name = rt-gcp
+    network = google_compute_network.network.selflink
+
+    rout {
+        source = ["0.0.0.0"]
+        cidr = ["172.16.1.0/24]
+    }
+}
 google_compute_instance "instance" {
-    name =
-    network =
-    subnetwork=
-    machine_type=
+    name = gcp-instance
+    network = google_compute_network.network.selflink
+    subnetwork= google_compute_subnetwork.sunetwork.id
+    machine_type= "e2-medium"
+    launch_public_ip = true
 }
